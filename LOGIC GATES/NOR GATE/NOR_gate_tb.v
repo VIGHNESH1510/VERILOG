9e8b556b8359cbe3cabaa4c91550c67236b8f9eb
Vighnesh E S <Vighnesh1510@gmail.com>
2026-02-01 20:20:47 +0530

    Create NOR_gate_tb.v

---
+++ LOGIC GATES/NOR GATE/NOR_gate_tb.v
@@ -0,0 +1,15 @@
+module NOR_gate_tb;
+  reg a,b;
+  wire out;
+  NOR_gate dut(.a(a),.b(b),.out(out));
+  initial begin
+    $monitor("time = %0t | a = %b | b = %b | out = %b",$time,a,b,out);
+    $dumpfile("NOR_tb.vcd");
+    $dumpvars(0,NOR_gate_tb);
+       a=0; b=0;
+    #10 a=0; b=1;
+    #10 a=1; b=0;
+    #10 a=1; b=1;
+    #10 $finish;
+  end
+endmodule
