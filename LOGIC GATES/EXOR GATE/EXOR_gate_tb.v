eb524f8fd1acab98fcb8871482ab03d7b21e8d38
Vighnesh E S <Vighnesh1510@gmail.com>
2026-02-01 20:25:51 +0530

    Create EXOR_gate_tb.v

---
+++ LOGIC GATES/EXOR GATE/EXOR_gate_tb.v
@@ -0,0 +1,15 @@
+module EXOR_gate_tb;
+  reg a,b;
+  wire out;
+  EXOR_gate dut(.a(a),.b(b),.out(out));
+  initial begin
+    $monitor("time = %0t | a = %b | b = %b | out = %b",$time,a,b,out);
+    $dumpfile("EXOR_tb.vcd");
+    $dumpvars(0,EXOR_gate_tb);
+       a=0; b=0;
+    #10 a=0; b=1;
+    #10 a=1; b=0;
+    #10 a=1; b=1;
+    #10 $finish;
+  end
+endmodule
