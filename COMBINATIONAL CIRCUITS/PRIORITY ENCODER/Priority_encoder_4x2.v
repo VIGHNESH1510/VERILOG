3f3ac76914f6914598ae4fd9b53fc8e88c6f32b6
Vighnesh E S <Vighnesh1510@gmail.com>
2026-02-04 13:17:46 +0530

    Update Priority_encoder_4x2.v

--- COMBINATIONAL CIRCUITS/PRIORITY ENCODER/Priority_encoder_4x2.v
+++ COMBINATIONAL CIRCUITS/PRIORITY ENCODER/Priority_encoder_4x2.v
@@ -1,6 +1,8 @@
 module priority_encoder(input [3:0]in,
-                        input v,
+                        output v,
                         output[1:0] y);
+  assign v= in==4'd0?0:1;
+  
   assign y = ((in[3] & v)?2'b11:
               (in[2] & v)?2'b10:
               (in[1] & v)?2'b01:
