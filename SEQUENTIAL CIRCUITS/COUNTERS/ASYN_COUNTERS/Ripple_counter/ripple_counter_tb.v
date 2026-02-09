cb1864125085d092f381d20071a3ffc25eb3e2ae
Vighnesh E S <Vighnesh1510@gmail.com>
2026-02-09 23:34:51 +0530

    Create ripple_counter_tb.v

---
+++ SEQUENTIAL CIRCUITS/COUNTERS/ASYN_COUNTERS/Ripple_counter/ripple_counter_tb.v
@@ -0,0 +1,16 @@
+module ripple_counter_tb#(parameter N=4);
+reg clk=0,reset;
+wire [N-1:0]Q;
+ripple_count rc(.clk(clk),.reset(reset),.Q(Q));
+always #5 clk=~clk;
+initial begin
+  $monitor("time=%0t reset=%b Q=%b",$time,reset,Q);
+  $dumpfile("Rc_tb.vcd");
+  $dumpvars;
+  reset=1;
+  #30 reset =0;
+  #30 reset=1;
+  #30 reset=0;
+  #100 $finish;
+end
+endmodule
