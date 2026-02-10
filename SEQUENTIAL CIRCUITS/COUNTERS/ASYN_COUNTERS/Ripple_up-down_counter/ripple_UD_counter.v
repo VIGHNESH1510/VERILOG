827aed6a60a9dd21ac78f1e3c31bf29b530e1e1a
Vighnesh E S <Vighnesh1510@gmail.com>
2026-02-10 22:01:14 +0530

    Update ripple_UD_counter.v
    new file added

--- SEQUENTIAL CIRCUITS/COUNTERS/ASYN_COUNTERS/Ripple_up-down_counter/ripple_UD_counter.v
+++ SEQUENTIAL CIRCUITS/COUNTERS/ASYN_COUNTERS/Ripple_up-down_counter/ripple_UD_counter.v
@@ -1,15 +1,11 @@
-module ripple_UD #(parameter N=4)(input clk,reset,mode,output reg [N-1:0]Q);
+module ripple_ud #(parameter N=4)(input clk,reset,mode,output reg [N-1:0]Q);
 always @(posedge clk or posedge reset)
 begin
-  if(reset && mode)
-    Q <= {4{1'b0}};
-  else if(reset && !mode)
-    Q <= {4{1'b1}};
+  if(reset)
+    Q <= {N{1'b0}};
   else if(mode)
     Q <= Q+1;
-  else if(!mode)
-    Q <= Q-1;
   else
-     Q <=Q;
+    Q <= Q-1;
 end
 endmodule
