de70288bcb06143db13813cea0722bd06ef2cfaf
Vighnesh E S <Vighnesh1510@gmail.com>
2026-02-10 22:02:57 +0530

    Update ripple_UD_counter_tb.v

--- SEQUENTIAL CIRCUITS/COUNTERS/ASYN_COUNTERS/Ripple_up-down_counter/ripple_UD_counter_tb.v
+++ SEQUENTIAL CIRCUITS/COUNTERS/ASYN_COUNTERS/Ripple_up-down_counter/ripple_UD_counter_tb.v
@@ -1,11 +1,18 @@
 module Ripple_UD_tb#(parameter N=4);
 reg clk=0,reset,mode;
 wire [N-1:0] Q;
-ripple_UD dut(.clk(clk),.reset(reset),.mode(mode),.Q(Q));
+ripple_ud dut(.clk(clk),.reset(reset),.mode(mode),.Q(Q));
 always #5 clk =~clk;
 initial begin
+  $dumpfile("rc_ud.vcd");
+  $dumpvars;
   $monitor("time=%0t reset=%b mode=%b Q=%b",$time,reset,mode,Q);
-  reset =0;
-  
+  reset =1;
+  mode =1;
+  #10 reset =0;
+  #25 mode =0;
+  #25 mode =1;
+  #100 $finish;
+
 end
 endmodule
