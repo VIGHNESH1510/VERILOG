e5c94caca29820d36fecdb489c3d76f8b832233e
Vighnesh E S <Vighnesh1510@gmail.com>
2026-02-11 22:35:38 +0530

    Create SISO.v

---
+++ SEQUENTIAL CIRCUITS/SHIFT REGISTERS/SISO/SISO.v
@@ -0,0 +1,12 @@
+module SISO #(parameter N=4)(input clk,reset,input data,output reg Q);
+reg [N-1:0]q;
+always @(posedge clk or posedge reset)
+begin
+  if(reset)
+    q <= 0;
+  else
+     q <= {q[N-2:0],data};
+  Q = q[N-1];
+
+end
+endmodule
