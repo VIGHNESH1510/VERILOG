1baf3498795b25fce0287fd68cd0ab3b5976c5ea
Vighnesh E S <Vighnesh1510@gmail.com>
2026-02-16 14:38:31 +0530

    Update MOD_N_COUNTER_tb.v

--- SEQUENTIAL CIRCUITS/COUNTERS/SYN_COUNTERS/MOD_N COUNTERS/MOD_N_COUNTER_tb.v
+++ SEQUENTIAL CIRCUITS/COUNTERS/SYN_COUNTERS/MOD_N COUNTERS/MOD_N_COUNTER_tb.v
@@ -1,16 +1,15 @@
 module N_bit_tb #(parameter N=4);
 reg clk=0,reset;
-reg [N-1:0] mod;
 wire [N-1:0] Q;
-N_bit_counters dut(.clk(clk),.reset(reset),.mod(mod),.Q(Q));
+N_bit_counters dut(.clk(clk),.reset(reset),.Q(Q));
 always #5 clk=~clk;
 initial begin
-  $monitor("time=%0t reset=%b mod=%b Q=%b",$time,reset,mod,Q);
+  $monitor("time=%0t reset=%b Q=%b",$time,reset,Q);
   $dumpfile("N_bit.vcd");
   $dumpvars;
-  reset=1;mod=10;
+  reset=1;
   #10 reset =0;
-  #100 $finish;
+  #120 $finish;
 
 end
 endmodule
