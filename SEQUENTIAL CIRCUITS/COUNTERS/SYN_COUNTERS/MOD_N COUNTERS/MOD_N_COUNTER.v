76684f5d6b12ec60347731eedf57235246ff3e65
Vighnesh E S <Vighnesh1510@gmail.com>
2026-02-16 14:37:57 +0530

    Update MOD_N_COUNTER.v

--- SEQUENTIAL CIRCUITS/COUNTERS/SYN_COUNTERS/MOD_N COUNTERS/MOD_N_COUNTER.v
+++ SEQUENTIAL CIRCUITS/COUNTERS/SYN_COUNTERS/MOD_N COUNTERS/MOD_N_COUNTER.v
@@ -1,7 +1,12 @@
-module N_bit_counters#(parameter N=4)(input clk,reset,input [N-1:0]mod,output reg [N-1:0]Q);
-always@(posedge clk )
+module N_bit_counters#(parameter N=4)(input clk,reset,output reg [N-1:0]Q);
+wire mod;
+assign mod= Q[3] & ~Q[2] & ~Q[1] & Q[0];
+
+//assign mod= Q[3] & ~Q[2] & Q[1] & ~Q[0];
+
+always@(posedge clk or posedge reset)
 begin
-  if(reset || Q==mod-1 || mod==0)
+  if(reset || mod)
     Q <= {N{1'b0}};
   else
     Q <= Q+1;
