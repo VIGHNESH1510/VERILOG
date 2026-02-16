37a3bafc1180004cf872145b6d9600340471d77d
Vighnesh E S <Vighnesh1510@gmail.com>
2026-02-16 18:01:22 +0530

    Create TFF_TO_DFF.v

---
+++ SEQUENTIAL CIRCUITS/FLIP FLOPS CONVERSION/D FF/TFF_TO_DFF.v
@@ -0,0 +1,12 @@
+module T_FF (input clk,reset,T,output reg Q);
+always @(posedge clk or posedge reset) begin
+  if(reset)
+    Q <=0;
+  else
+     Q<= T? ~Q:Q;
+end
+endmodule
+
+module D_FF(input clk,reset,D,output Q);
+T_FF t(.clk(clk),.reset(reset),.T(D^Q),.Q(Q));
+endmodule
