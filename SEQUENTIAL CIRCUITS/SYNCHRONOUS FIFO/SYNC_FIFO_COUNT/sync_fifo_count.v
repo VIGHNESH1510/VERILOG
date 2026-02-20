a0df2f8a714e755eabe9ae05499bc1eb88ef7749
Vighnesh E S <Vighnesh1510@gmail.com>
2026-02-20 22:59:42 +0530

    Create sync_fifo_count.v

---
+++ SEQUENTIAL CIRCUITS/SYNCHRONOUS FIFO/SYNC_FIFO_COUNT/sync_fifo_count.v
@@ -0,0 +1,32 @@
+module syn_fifo(input clk,reset,w_en,r_en,input [1:0]data,output reg [1:0]d_out,output full,empty);
+reg [1:0]r_ptr,w_ptr;
+reg [1:0] mem[3:0];
+reg [2:0] count;
+always @(posedge clk or posedge reset)
+begin
+  if(reset) begin
+    d_out <=0;
+    r_ptr <=0;
+    w_ptr <=0;
+    count <=0;
+  end
+  else begin
+    if(w_en && !full)
+    begin
+      mem[w_ptr] <= data;
+      w_ptr <= w_ptr+1;
+      count <= count+1;
+    end
+
+    if(r_en && !empty)
+    begin
+      d_out <= mem[r_ptr];
+      r_ptr <= r_ptr+1;
+      count <= count-1;
+    end
+  end
+end
+assign full = (count == 4);
+assign empty = (count == 0);
+
+endmodule
