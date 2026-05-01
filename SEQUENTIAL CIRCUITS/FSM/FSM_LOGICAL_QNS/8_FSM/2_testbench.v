ab565ba60467d2d4cf15a17dfd8cd649946db20a
Vighnesh E S <Vighnesh1510@gmail.com>
2026-05-01 23:05:55 +0530

    Create 2_testbench.v

---
+++ SEQUENTIAL CIRCUITS/FSM/FSM_LOGICAL_QNS/8_FSM/2_testbench.v
@@ -0,0 +1,53 @@
+module tb;
+  reg clk,reset,in;
+  wire out;
+  fsm dut(clk,reset,in,out);
+  always #5 clk =~clk;
+  always @(posedge clk)
+    #1 $display("time=%0t reset= %b in = %b out = %b",$time,reset,in,out);
+  initial begin
+    //$monitor("time=%0t in = %b out = %b",$time,in,out);
+    $dumpfile("tb.vcd");
+    $dumpvars(0,tb);
+    clk =0; reset=1; in=0;
+    #12
+    reset =0;
+    #10;
+    in = 1;
+    #10;
+    in =1;
+    #10;
+    in =1;
+    #10;
+    in =1;
+    #10; in =0;
+    #10;
+    in = 1;
+    #10;
+    in =1;
+    #10;
+    in =1;
+    #10;
+    in =1;
+    #10; in =0;
+    #10;
+    $finish;
+    
+  end
+endmodule
+
+/*
+time=6 reset= 1 in = 0 out = 0
+time=16 reset= 0 in = 0 out = 0
+time=26 reset= 0 in = 1 out = 0
+time=36 reset= 0 in = 1 out = 1
+time=46 reset= 0 in = 1 out = 1
+time=56 reset= 0 in = 1 out = 1
+time=66 reset= 0 in = 0 out = 0
+time=76 reset= 0 in = 1 out = 0
+time=86 reset= 0 in = 1 out = 1
+time=96 reset= 0 in = 1 out = 1
+time=106 reset= 0 in = 1 out = 1
+time=116 reset= 0 in = 0 out = 0
+testbench.sv:36: $finish called at 122 (1s)
+*/
